891e7e7e5e544a1861cef7453a324c4267d6dec7
eluser <studenteliot1999@gmail.com>
2022-10-11 23:27:58 +0300

    Added functions for prices, side bar

--- main.js
+++ main.js
@@ -24,24 +24,37 @@ function ready(){
     }
 
     var quantityInputs = document.getElementsByClassName('cart-quantity')
+    for(var i = 0; i < quantityInputs.length; i++){
+        var input = quantityInputs[i];
+        input.addEventListener('change', quantityChanged)
+    }
 }
 
 function removeCartItem(){
     var buttonClicked = event.target;
     buttonClicked.parentElement.remove()
-    updateTotal()
+    updatetotal()
 }
 
-function updateTotal(){
+function quantityChanged(event){
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0){
+        input.value = 1
+    }
+    updatetotal()
+}
+
+
+function updatetotal(){
     var cartContent = document.getElementsByClassName('cart-content')[0]
     var cartBoxes = cartContent.getElementsByClassName('cart-box')
     var total = 0;
     
-    for(var i = 0; i < removeBoxes.length; i++){
+    for(var i = 0; i < cartBoxes.length; i++){
         var cartBox = cartBoxes[i]
         var priceElement = cartBox.getElementsByClassName('cart-price')[0]
         var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0]
-        var price = parseFLoat(priceElement.innerText.replace("$", ""))
+        var price = parseFLoat(priceElement.innerText.replace("$", " "))
         var quantity = quantityElement.value;
         total = total + (price * quantity);
 
